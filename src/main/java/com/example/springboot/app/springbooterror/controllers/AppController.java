102e0c0480ac2ad5e6b678cec0dc7c976fdde3ff
Francisco González Fernández <franciscogf@agrocontratacion.com>
2023-06-27 16:13:04 +0200

    init

---
+++ src/main/java/com/example/springboot/app/springbooterror/controllers/AppController.java
@@ -0,0 +1,15 @@
+package com.example.springboot.app.springbooterror.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+  @GetMapping({ "/index" })
+  public String index() {
+    
+    return "index";
+  }
+  
+}
